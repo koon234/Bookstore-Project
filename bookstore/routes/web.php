b9b81e6ad6c16993c478502b04d6871f3444c3c0
Aishastela <101183583+Aishastela@users.noreply.github.com>
2024-04-07 21:51:35 +0630

    registration route

--- bookstore/routes/web.php
+++ bookstore/routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthenticationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/registration', [AuthenticationController::class, 'registration'])->name('registration');
 Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('adminDashboard');
