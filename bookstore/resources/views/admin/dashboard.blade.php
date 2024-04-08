7cde4104c3c6bbc1f770d28e324e3714f218bb89
Aishastela <101183583+Aishastela@users.noreply.github.com>
2024-04-08 22:14:30 +0630

    Admin Panel design

--- bookstore/resources/views/admin/dashboard.blade.php
+++ bookstore/resources/views/admin/dashboard.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin</title>
+    <!-- Link to your CSS file -->
+    <link href="{{ asset('css/adminPanel.css') }}" rel="stylesheet">
     <!-- fontawsome css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
